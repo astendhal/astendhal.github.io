3f5de8294038ec595d8a26185b6e44e8813abf53
astendhal <150939155+astendhal@users.noreply.github.com>
2025-06-25 10:23:09 +0800

    main.js

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ function calculate() {
 	bmi = bmi.toFixed(1);
 
 	if (bmi <= 18.4) {
-		measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
+		measure = "你的BMI值为 " + bmi + " 你的体重过轻 " ;
 	} else if (bmi >= 18.5 && bmi <= 24.9) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Normal";
+		measure = "Your BMI is " + bmi + " 你的体重正常 " ;
 	} else if (bmi >= 25 && bmi <= 29.9) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
+		measure = "Your BMI is " + bmi + " 你的体重超重 " ;
 	} else if (bmi >= 30) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
+		measure = "Your BMI is " + bmi + " 你的体重已达到肥胖程度 " ;
 	}
 	
 
